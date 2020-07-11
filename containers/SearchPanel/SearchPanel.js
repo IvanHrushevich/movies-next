fc5f07895a2bdffc8590a8d5fb0693ee78ae627e
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-07-12 02:58:55 +0300

    feat: implement searching

--- containers/SearchPanel/SearchPanel.js
+++ containers/SearchPanel/SearchPanel.js
@@ -1,40 +1,45 @@
+import { useRouter } from 'next/router';
+
 import classes from './SearchPanel.module.scss';
 import SortPanel from '../SortPanel/SortPanel';
 import { Logo, SearchControl, SortControl } from '../../components';
 
-const SearchPanel = (props) => (
-  <>
-    <header className={classes.sectionHeader}>
-      <Logo />
-      <div className={`container-global ${classes.searchBlock}`}>
-        <p className={classes.title}>FIND YOUR MOVIE</p>
-        <div className={classes.searchControl}>
-          <SearchControl
-            onSearchClick={(searchStr) => {
-              props.history.push(`/search/${searchStr}`);
+const SearchPanel = () => {
+  const router = useRouter();
+  return (
+    <>
+      <header className={classes.sectionHeader}>
+        <Logo />
+        <div className={`container-global ${classes.searchBlock}`}>
+          <p className={classes.title}>FIND YOUR MOVIE</p>
+          <div className={classes.searchControl}>
+            <SearchControl
+              onSearchClick={(searchStr) => {
+                router.push(`/search/title/${searchStr}`);
+              }}
+            />
+          </div>
+          <SortControl
+            title="search by"
+            btnLeft={{
+              label: 'title',
+              value: 'title',
+              checked: true,
+            }}
+            btnRight={{
+              label: 'genre',
+              value: 'genres',
+            }}
+            inputName="searchBy"
+            onChange={(event) => {
+              // props.changeSearchBy(event.target.value);
             }}
           />
         </div>
-        <SortControl
-          title="search by"
-          btnLeft={{
-            label: 'title',
-            value: 'title',
-            checked: true,
-          }}
-          btnRight={{
-            label: 'genre',
-            value: 'genres',
-          }}
-          inputName="searchBy"
-          onChange={(event) => {
-            props.changeSearchBy(event.target.value);
-          }}
-        />
-      </div>
-    </header>
-    <SortPanel />
-  </>
-);
+      </header>
+      <SortPanel />
+    </>
+  );
+};
 
 export default SearchPanel;
