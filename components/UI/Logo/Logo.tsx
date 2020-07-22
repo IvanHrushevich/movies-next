6cc45357f750c8d5ed3111a88e2289d9922bcddc
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-07-23 00:49:03 +0300

    add story for Logo

--- components/UI/Logo/Logo.tsx
+++ components/UI/Logo/Logo.tsx
@@ -1,12 +1,28 @@
-import { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
+import { createUseStyles } from 'react-jss';
 
 import classes from './Logo.module.scss';
 
-const Logo: FunctionComponent = () => (
-  <div className={classes.container}>
-    <span className={classes.titleBold}>netflix</span>
-    <span>roulette</span>
-  </div>
-);
+const useStyles: any = createUseStyles({
+  container: {
+    fontSize: '2rem',
+    color: '#f65261',
+  },
+
+  titleBold: {
+    fontWeight: '800',
+  },
+});
+
+const Logo: FunctionComponent = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <span className={classes.titleBold}>netflix</span>
+      <span>roulette</span>
+    </div>
+  );
+};
 
 export default Logo;
---
+++ stories/1-Logo.stories.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+
+import Logo from '../components/UI/Logo/Logo';
+
+export default {
+  title: 'Logo',
+  component: Logo,
+};
+
+export const basic: FunctionComponent = () => <Logo />;
